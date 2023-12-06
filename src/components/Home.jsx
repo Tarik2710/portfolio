ef6e684e830bbd31d6ec44c7523a91966cd630f8
unknown <tarik.bijedic@yahoo.com>
2023-12-06 21:01:00 +0100

    R8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   <div className="xl:w-1/2 lg:w-1/2 xl:mb-25 lg:mb-20  md:w-full sm:w-full  p-8 text-center">
     <p className='xl:my-5 xl:text-4xl xl:text-left lg:text-left lg:text-4xl sm:text-3xl md:text-4xl text-[#f5f3f4] font-medium'>Hi, I'm</p>
         <div className='xl:my-5 xl:text-left lg:text-left'>
-        <TypeAnimation className='text-[#f5f3f4] font-medium lg:text-6xl md:text-4xl sm:text-4xl'
+        <TypeAnimation className='text-[#f5f3f4] font-medium lg:text-6xl md:text-4xl sm:text-[33px]'
         sequence={[
         'Bijedić Tarik',
         1500,]}
@@ -25,7 +25,7 @@ const Home = () => {
         cursor={false} 
         /></div>
         {showSecondAnimation && (<div className='xl:my-5 lg:text-left xl:text-lg xl:text-left sm:text-2xl sm:text-center'>
-        <TypeAnimation className='text-[#f5f3f4] lg:text-7xl md:text-5xl sm:text-4xl mt-5'
+        <TypeAnimation className='text-[#f5f3f4] lg:text-7xl md:text-5xl sm:text-[33px] mt-5'
         sequence={[
         'Frontend Developer',
         1500,]}
@@ -43,7 +43,7 @@ const Home = () => {
     <img
       src={computerUsers}
       alt="Opis slike"
-      className="object-cover max-h-1/2 lg:w-[700px] lg:mx-auto md:mx-auto sm:w-[500px] sm:mx-auto xl:ml-10 md:h-full"
+      className="object-cover max-h-1/2 xxl:w-[800px] xxl:mx-auto lg:w-[700px] lg:mx-auto md:mx-auto sm:w-[500px] sm:mx-auto xl:ml-10 md:h-full"
     />
   </div>
 </div>
