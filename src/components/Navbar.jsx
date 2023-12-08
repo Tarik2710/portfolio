37f0208c038e999b6099217e8bbc8b9ca31fcf19
unknown <tarik.bijedic@yahoo.com>
2023-12-08 01:19:53 +0100

    R7-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,27 +14,28 @@ const Navbar = () => {
   return (
     <div className=' w-full h-[90px] flex justify-between items-center px-5   bg-[#181818] text-[#edf2f4] border-b-4 border-[#4b4f58] p-4'>
       <div className='sm:w-40'>
-        <img src={Logo} alt='Logo' className='object-contain w-16 pt-2 mb-2 hover:scale-105'/>
-      </div>
+        <a href='https://tarikbijedic.netlify.app/'><img src={Logo} alt='Logo' className='object-contain w-16 pt-2 mb-2 transition duration-500 ease-in-out hover:scale-105'/>
+        </a></div>
 
       {/* menu */}
       <ul className='hidden md:flex text-2xl font-semibold'>
-        <li className='hover:text-[#bc6c25]'>
+        <li className='hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110 '>
           <Link to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='hover:text-[#bc6c25]'>
+
+        <li className=' hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110 '>
           <Link to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='hover:text-[#bc6c25]'>
+        <li className='hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110 '>
           <Link to='portfolio' smooth={true} duration={500}>
             Portfolio
           </Link>
         </li>
-        <li className='hover:text-[#bc6c25]'>
+        <li className='hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
           <Link to='contact' smooth={true} duration={500}>
             Contact
           </Link>
@@ -42,7 +43,7 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 text-3xl font-semibold hover:text-[#bc6c25]'>
+      <div onClick={handleClick} className='md:hidden z-10 text-3xl font-semibold hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
@@ -54,24 +55,24 @@ const Navbar = () => {
             : 'absolute top-0 left-0 w-full h-screen bg-[#0A1828] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl hover:bg-[#2d5252]'>
+        <li className='py-6 text-4xl hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl  hover:bg-[#2d5252]'>
+        <li className='py-6 text-4xl hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
           {' '}
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl hover:bg-[#2d5252]'>
+        <li className='py-6 text-4xl hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
           {' '}
           <Link onClick={handleClick} to='portfolio' smooth={true} duration={500}>
             Portfolio
           </Link>
         </li>
-        <li className='py-6 text-4xl hover:bg-[#2d5252]'>
+        <li className='py-6 text-4xl hover:text-sky-400 active:text-to-blue-800 transition duration-500 ease-in-out hover:scale-110'>
           {' '}
           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,8 +23,8 @@ const Projects = () => {
                           ease={[0.75, 0.45, 0.53, 0.94]}
                           reset={true}>
       <div className="flex flex-wrap justify-center overflow-hidden xxl:mx-[300px] ultraxl:mx-[500px] xl:mx-[100px] lg:mx-0">
-        <div className="bg-gradient-to-br from-gray-700 via-gray-900 to-black rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-l-2 border-2  border-solid border-[#ffff] mt-3">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-gradient-to-br from-gray-700 via-gray-900 to-black rounded-md p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 mt-3 hover:bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] hover:duration-1000 ">
+         
           <div className="mb-4">
             <img
               src={pageone}
@@ -32,19 +32,19 @@ const Projects = () => {
               className="object-cover w-full h-40 rounded-t-md cursor-pointer"
             />
           </div>
-          {/* Sadržaj kartice */}
+          
           <div>
-            <p className="text-lg mb-4 text-[#ffff] max-w-[300px] font-medium text-justify">
-              John Doe Portfolio, use plain HTML and CSS. This is a long paragraph to demonstrate text wrapping.
+            <p className="text-lg text-[#ffff] max-w-[300px] mb-10 font-medium text-justify">
+              John Doe Portfolio, use plain HTML and CSS. 
             </p>
-            <a href="https://github.com/Tarik2710/johndoeportfolio" target="_blank"  rel="noreferrer" ><button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black py-2 px-4 rounded mx-auto block overflow-hidden hover:scale-105 transition duration-500 ">
+            <a href="https://github.com/Tarik2710/johndoeportfolio" target="_blank"  rel="noreferrer" ><button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black py-2 px-4 rounded mx-auto block overflow-hidden transition duration-700 ease-in-out hover:scale-105 ">
               SEE MORE
             </button></a>
           </div>
         </div>
 
-        <div className="bg-gradient-to-l from-gray-700 via-gray-900 to-black rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-1 border-solid border-[#ffff] mt-3 border-2">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-gradient-to-l from-gray-700 via-gray-900 to-black rounded-md p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 mt-3 hover:bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] hover:duration-1000">
+          
           <div className="mb-4">
             <img
               src={pagetwo}
@@ -52,19 +52,20 @@ const Projects = () => {
               className="object-cover w-full h-40 rounded-t-md"
             />
           </div>
-          {/* Sadržaj kartice */}
+          
           <div>
-            <p className="text-lg mb-4  font-medium max-w-[300px] text-justify">
-              Relvise Finance and Consultancy Solution, use HTML, Bootstrap and CSS.
+            <p className="text-lg mb-4  font-medium max-w-[300px] text-justify ">
+              Relvise Finance and Consultancy Solution, use HTML CSS and Bootstrap.
             </p>
-            <button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black py-2 px-4 rounded mx-auto block">
+            <a href="https://github.com/Tarik2710/relvise" target="_blank"  rel="noreferrer" >
+            <button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black py-2 px-4 rounded mx-auto block transition duration-700 ease-in-out hover:scale-105">
               SEE MORE
-            </button>
+            </button></a>
           </div>
         </div>
 
-        <div className="bg-gradient-to-tl from-gray-700 via-gray-900 to-black  rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-1 border-solid border-[#ffff] mt-3 border-r-2 border-2">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-gradient-to-tl from-gray-700 via-gray-900 to-black  rounded-md p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 mt-3 hover:bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] hover:duration-1000">
+          
           <div className="mb-4">
             <img
               src={pagethree}
@@ -72,19 +73,20 @@ const Projects = () => {
               className="object-cover w-full h-40 rounded-t-md  cursor-pointer"
             />
           </div>
-          {/* Sadržaj kartice */}
+        
           <div>
             <p className="text-lg mb-4  font-medium max-w-[300px] text-justify">
-              Primax, page for digital agency, use HTML, CSS and Bootstrap.
+              Primax digital agency, use HTML CSS and Bootstrap.
             </p>
-            <button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black  mt-10 py-2 px-4 rounded mx-auto block">
+            <a href="https://github.com/Tarik2710/primax" target="_blank"  rel="noreferrer" >
+            <button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black  mt-10 py-2 px-4 rounded mx-auto block transition duration-700 ease-in-out hover:scale-105 ">
               SEE MORE
-            </button>
+            </button></a>
           </div>
         </div>
 
-        <div className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-l-2 border-2 border-solid border-[#ffff]">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 ">
+          
           <div className="mb-4">
             <img
               src={pagefour}
@@ -92,18 +94,19 @@ const Projects = () => {
               className="w-full h-40 rounded-t-md cursor-pointer border-spacing-10"
             />
           </div>
-          {/* Sadržaj kartice */}
+         
           <div>
             <p className="text-lg mb-4  font-medium max-w-[300px] text-justify ">
-              Travel Agency, use HTML, Bootstrap and CSS for style. This is a long paragraph to demonstrate text wrapping.
+              Travel Star Agency use HTML CSS and Bootstrap.
             </p>
-            <button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black py-2 px-4 rounded mx-auto block">
+            <a href="https://github.com/Tarik2710/travelstar" target="_blank"  rel="noreferrer" >
+            <button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black py-2 px-4 rounded mx-auto block transition duration-700 ease-in-out hover:scale-105">
               SEE MORE
-            </button>
+            </button></a>
           </div>
         </div>
-        <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black  rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-solid border-[#ffff] border-2">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black  rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 hover:bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] hover:duration-1000">
+     
           <div className="mb-4">
             <img
               src={pagesix}
@@ -111,18 +114,19 @@ const Projects = () => {
               className="object-cover w-full h-40 rounded-t-md  cursor-pointer border-spacing-10"
             />
           </div>
-          {/* Sadržaj kartice */}
+     
           <div>
             <p className="text-lg mb-4  font-medium max-w-[300px] text-justify">
-              Travel Agency, use HTML, Bootstrap and CSS for style. This is a long paragraph to demonstrate text wrapping.
+            Architecture Websites Examples use HTML, CSS and Bootstrap
             </p>
-            <button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black py-2 px-4 rounded mx-auto block">
+            <a href="https://github.com/Tarik2710/lorem" target="_blank"  rel="noreferrer" >
+            <button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black py-2 px-4 rounded mx-auto block transition duration-700 ease-in-out hover:scale-105">
               SEE MORE
-            </button>
+            </button></a>
           </div>
         </div>
-        <div className="bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black  rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition duration-500 border-r-2 border-2 border-solid border-[#ffff]">
-          {/* Gornji deo slike (ceo gornji deo) */}
+        <div className="bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black  rounded-md shadow-2xl p-8 mb-8 mx-auto overflow-hidden hover:scale-105 transition hover:bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] hover:duration-1000">
+        
           <div className="mb-4">
             <img
               src={pagefive}
@@ -130,14 +134,15 @@ const Projects = () => {
               className="object-cover w-full h-40 rounded-t-md cursor-pointer border-spacing-10"
             />
           </div>
-          {/* Sadržaj kartice */}
+
           <div>
             <p className="text-lg mb-4 font-medium max-w-[300px] text-justify ">
-              Travel Agency, use HTML, Bootstrap and CSS for style. This is a long paragraph to demonstrate text wrapping.
+              Digital Agency, used HTML, CSS, Bootstrap and JS for animate text.
             </p>
-            <button className="bg-[#ffff] hover:bg-[#2d5252]  text-black uppercase font-black py-2 px-4 rounded mx-auto block">
+            <a href="https://github.com/Tarik2710/digital" target="_blank"  rel="noreferrer" >
+            <button className="bg-[#ffff] hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800  text-black uppercase font-black py-2 px-4 rounded mx-auto block transition duration-700 ease-in-out hover:scale-105 ">
               SEE MORE
-            </button>
+            </button></a>
           </div>
         </div>
       </div>
