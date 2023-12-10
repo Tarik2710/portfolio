49615e12f9b42b6364f85019a067ec1f70095637
unknown <tarik.bijedic@yahoo.com>
2023-12-10 03:06:28 +0100

    R8-1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import osoba from '../assets/covjek.jpg';
-import { MotionAnimate } from 'react-motion-animate'
+import { motion, AnimatePresence } from "framer-motion"
 import Pdf from '../assets/cv.pdf';
 
 
@@ -11,11 +11,8 @@ const About = () => {
   return (
     <div className='bg-[#181818]' name='about'>
 
-      <MotionAnimate
-        animation='scrollOpacity'
-        ease={[0.17, 0.67, 0.97, -0.48]}
-        scrollPositions={[0.1, 0.5, 0.6, 0.8]}>
-        <div className="w-full min-h-screen flex flex-col md:flex-row items-center justify-center bg-[#181818] relative overflow-hidden shadow-md border-b-4 border-[#4b4f58]">
+     
+        <div className="transition-opacity ease-in duration-700 animate-fade-up animate-duration-1000 animate-delay-500 w-full min-h-screen flex flex-col md:flex-row items-center justify-center bg-[#181818] relative overflow-hidden shadow-md border-b-4 border-[#4b4f58]">
 
           <div className="md:w-1/2 relative z-10 p-8 ">
             <div className="bg-dda15e rounded-lg overflow-hidden border-10 shadow-2xl shadow-black">
@@ -30,7 +27,7 @@ const About = () => {
 
           <div className="md:w-1/2 xl:p-8 lg:p-8 md:p-8 sm:p-4 text-[#f5f3f4] font-medium text-center relative z-20  ">
             <h2 className="text-4xl md:text-7xl sm:text-3xl font-bold lg:mb-10 sm:mb-5  shadow-lg ">ABOUT ME</h2>
-            <p className="xl:mb-10 lg:mb-10 text-justify text-xl font-bold sm:text-base sm:mb-5 ">Hello, I am  Tarik Bijedić. I am currently a junior web developer. My knowledge is based on HTML, CSS and JAVASCRIPT, proficient in using frameworks such as Tailwind, Bootstrap, and ReactJS. What motivated my journy in IT world was wish, to understand mechanics behind what happens when we interact with a digital interface. One of the appealing aspects of jobs in this field is the potential for a substantial salary. My main goal is making dynamic and funcional pages, providing users with great online experience. I have hands-on experience with Content Management Systems (CMS) and have successfully worked with platforms like WordPress and PrestaShop. What makes this world more intriguing is the fact that there is allways more to learn, and by that progress and engagement in this field is undeniable to me.</p>
+            <p className="xl:mb-10 lg:mb-10 text-justify text-xl font-bold sm:text-base sm:mb-5 ">Hello, I am  Tarik Bijedić. I am currently a junior web developer. My knowledge is based on HTML, CSS and JavaScript, proficient in using frameworks such as Tailwind, Bootstrap, and ReactJS. What motivated my journy in IT world was wish, to understand mechanics behind what happens when we interact with a digital interface. One of the appealing aspects of jobs in this field is the potential for a substantial salary. My main goal is making dynamic and funcional pages, providing users with great online experience. I have hands-on experience with Content Management Systems (CMS) and have successfully worked with platforms like WordPress and PrestaShop. What makes this world more intriguing is the fact that there is allways more to learn, and by that progress and engagement in this field is undeniable to me.</p>
             <a href={Pdf} without rel="noopener noreferrer" target="_blank"><button trailingIcon="picture_as_pdf" label="Resume" className="bg-[#ffffff] hover:text-white hover:bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 transition duration-700 ease-in-out hover:scale-110  text-black uppercase font-black py-2 px-4 rounded shadow-lg hover:shadow-2xl sm:mb-16 sm:mt-10 lg:mt-5">
               See My CV
             </button></a>
@@ -46,7 +43,6 @@ const About = () => {
             />
           </div>
         </div>
-      </MotionAnimate>
     </div>
   );
 };
