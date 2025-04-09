40b9cab1e9c7b2c601dc4e172e02839968a457ae
Tarik <135732726+Tarik2710@users.noreply.github.com>
2025-04-09 09:07:31 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-neutral-300 p-2 text-center
 text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200 flex justify-center items-center">
-      <p className='mr-3'>© 2023 Bijedić Tarik</p>
+      <p className='mr-3'>© 2025 Bijedić Tarik</p>
     <div className='flex items-center justify-center'><Icon/></div> 
     </div>
 );
